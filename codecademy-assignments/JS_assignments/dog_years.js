b9390d957961fdccbee2687b096ba7b271570c97
kiddulu916 <70697634+kiddulu916@users.noreply.github.com>
2024-08-12 08:12:20 -0700

    added dog years assignment

---
+++ codecademy-assignments/JS_assignments/dog_years.js
@@ -0,0 +1,23 @@
+// Setting my age to a variable
+var myAge = 34
+
+// Setting variable for the first 2 years of life
+let earlyYears = 2
+
+// Converting the first 2 years of life to dog years
+earlyYears = earlyYears * 10.5
+
+// Setting a variable for the later years after 2
+let laterYears = myAge - 2
+
+// converting the later years of life to dog years
+laterYears = laterYears * 4
+
+// Calculating my age in dog years
+var myAgeInDogYears = earlyYears + laterYears
+
+// Setting string of my name to a variable
+var myName = 'Corey Scott Hilsenbeck'.toLowerCase()
+
+// Interpolate my name and age in dog years
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
